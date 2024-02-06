84aabd4b81fa3a974b682157518b8e8679f3bd5b
Lukas Raska <luky.raska@gmail.com>
2024-02-06 11:42:55 +0100

    Task 6 solved

---
+++ src/main/java/task_0006/Main.java
@@ -0,0 +1,40 @@
+package task_0006;
+
+//The sum of the squares of the first ten natural numbers is: 1(pow2) + 2(pow2) + ... + 10(pow2) = 385
+
+//The square of the sum of the first ten natural numbers is: (1+2+...+10)(pow2) = 55(pow2) = 3025
+//
+//Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is:
+// 3025-385 = 2640
+//.
+//
+//Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    private static final int LIMIT = 100;
+
+    private static int getSumOfSquares(int limit) {
+        return IntStream
+                .rangeClosed(1, limit)
+                .map(n -> n * n)
+                .sum();
+    }
+
+    private static int getSquareOfTheSum(int limit) {
+        int sum = IntStream
+                .rangeClosed(1, limit)
+                .sum();
+        return sum * sum;
+    }
+
+    public static void main(String[] args) {
+
+        int difference = getSquareOfTheSum(LIMIT) - getSumOfSquares(LIMIT);
+
+        System.out.println("Answer: " + difference);
+
+    }
+}
